ef61ec94d9563c0474b5746d7b76050149546e05
Kristian Bonnici <kristian.bonnici@aalto.fi>
2021-06-26 23:30:17 +0200

    Bump version: 0.0.2 → 0.0.3

--- mlvizlib/__init__.py
+++ mlvizlib/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kristian Bonnici"""
 __email__ = 'kristiandaaniel@gmail.com'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from mlvizlib import classification
 #from mlvizlib.classification import confusion_matrix
