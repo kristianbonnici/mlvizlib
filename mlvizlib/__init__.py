ba75e8b5e7d0dcd305d96e96861ce44814bb754c
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-07-05 23:49:02 +0200

    Bump version: 0.1.2 → 0.1.3

--- mlvizlib/__init__.py
+++ mlvizlib/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kristian Bonnici"""
 __email__ = 'kristiandaaniel@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 # from mlvizlib import classification
 # from mlvizlib import utils
